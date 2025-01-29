ebfe35e7790af34a51fbafc1459e27c85732a8d2
MarieB <marie.brice16@orange.fr>
2025-01-29 11:33:00 +0100

    ajout composant style background gradient

--- styles/gradientBackground.js
+++ styles/gradientBackground.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { signPageStyles } from '../styles/signPageStyles';
 
-const GradientBackground = ({ children }) => {
+const GradientBackground = ({ children, colors }) => {
   return (
     <LinearGradient
-        colors={['rgba(21, 187, 216, 0.3)', 'rgba(85, 0, 255, 0.3)']}
+        colors={colors || ['rgba(21, 187, 216, 0.3)', 'rgba(85, 0, 255, 0.3)']}
         start={{ x: 0, y: 0.1 }}
         end={{ x: 0, y: 1 }}
         style={signPageStyles.gradient}
